6ecdd3880f88daf144d24deaa4df16ffee3d23a7
Andreas Zeidler <az@zitc.de>
2020-04-03 13:16:39 +0200

    Add Portainer so we can see logs and control Docker from a UI
    See https://www.portainer.io/

--- tests/services.rb
+++ tests/services.rb
@@ -13,3 +13,15 @@ describe docker_service('pihole_pihole') do
   its('ports') { should include '*:8053->80/tcp' }
   its('ports') { should include '*:443->443/tcp' }
 end
+
+describe docker_service('portainer_agent') do
+  it { should exist }
+  its('mode') { should eq 'global' }
+  its('ports') { should eq '' }
+end
+
+describe docker_service('portainer_portainer') do
+  it { should exist }
+  its('ports') { should include '*:8000->8000/tcp' }
+  its('ports') { should include '*:9000->9000/tcp' }
+end
